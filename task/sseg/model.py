f17713962dbb258dea7525f09bab10bd7a48ae60
Jeremiah Coholich <jmcoholich@gmail.com>
2022-11-15 23:12:20 -0500

    divide by folded ones so that logits are correct for training, add code for labelMapToIm, add dynamic stride (commented out, might be bugs)

--- task/sseg/model.py
+++ task/sseg/model.py
@@ -9,7 +9,16 @@ from module import deeplab_v2, _pspnet
 
 from torchvision.utils import save_image
 
+def labelMapToIm(label, label_map):
+    """
+    label: H, W, 1 tensor
+    label_map: [[[r, g, b], index]]...]
+    """
+    output = label.repeat(1, 1, 3)
+    for color, id in label_map:
+        output[label.squeeze(2)==id] = torch.tensor(color)
 
+    return output
 
 def add_parser_arguments(parser):
     pixelssl.model_template.add_parser_arguments(parser)
@@ -59,30 +68,38 @@ class DeepLab(pixelssl.model_template.TaskModel):
 
         self.batch, self.channels, self.rows, self.columns = inp.shape
         self.output_classes = 21
-        # num_windows = 6
-        # assert rows == columns  # TODO this should be case once we fix the preprocessor to apply to same crop to the teacher image
-        # self.kernel_size = 321 # TODO pass all of these in from the config files
-        self.kernel_size = 321 # TODO pass all of these in from the config files
-        self.padding = 300 # TODO
-        self.stride = 50
-        # img is 769x769   # TODO will I have to upsample after sliding? If not, have we considering trying that?
+        self.kernel_size = 321
+        # des_stride = 25
+        # # import pdb; pdb.set_trace()
+        # n_windows_row = max(int((self.rows - des_stride) / (self.kernel_size - des_stride)) + 1, 2)
+        # n_windows_col = max(int((self.columns - des_stride) / (self.kernel_size - des_stride)) + 1, 2)
+        # self.stride = (int((n_windows_row * self.kernel_size - self.rows)
+        #                    / (n_windows_row - 1)),
+        #                int((n_windows_col * self.kernel_size - self.rows)
+        #                    / (n_windows_col - 1)))
+        # if self.rows < self.kernel_size:
+        #     padding_rows = int((self.kernel_size - self.rows) / 2) + 1
+        # else:
+        #     padding_rows = 0
+        # if self.columns < self.kernel_size:
+        #     padding_cols = int((self.kernel_size - self.columns) / 2) + 1
+        # else:
+        #     padding_cols = 0
+        # self.padding = (padding_rows, padding_cols)
+        self.padding = 100
+        self.stride = 25
         inp = nn.functional.unfold(
             inp,
             kernel_size=(self.kernel_size, self.kernel_size),
             padding=self.padding,
-            stride=self.stride) # TODO hard pytroch
-        # print("after unfold:", inp.shape)
+            stride=self.stride)
         self.num_windows = inp.shape[-1]
         inp = inp.permute((0, 2, 1))
-        # print("after permute:", inp.shape)
         inp = inp.reshape(self.batch * self.num_windows, self.channels, self.kernel_size, self.kernel_size)
 
         # for i in range(inp.shape[0]):
         #     img1 = inp[i]
         #     save_image(img1, f'pics/after_sliding_{i}.png')
-
-
-        # print("after reshape:", inp.shape)
         return inp
 
     def unmake_sliding_windows(self, pred, latent):
@@ -92,20 +109,39 @@ class DeepLab(pixelssl.model_template.TaskModel):
         #     save_image(img1, f'pics/pred_before_fold_{i}.png')
         # print("teacher output[0]:", teacher_output_0.shape)
         pred = pred.reshape(self.batch, self.num_windows, self.output_classes * self.kernel_size**2)
-        # print("teacher output[0] reshape:", pred.shape)
         pred = pred.permute((0, 2, 1))
-        # print("teacher output[0] permute:", pred.shape)
+        temp = torch.ones_like(pred)
         pred = nn.functional.fold(
             pred,
             output_size=(self.rows, self.columns),
             kernel_size=(self.kernel_size, self.kernel_size),
             padding=self.padding,
             stride=self.stride)
+        temp = nn.functional.fold(
+            temp,
+            output_size=(self.rows, self.columns),
+            kernel_size=(self.kernel_size, self.kernel_size),
+            padding=self.padding,
+            stride=self.stride)
+        # print("temp shape: ", temp.shape)
+        # temp = temp[:, 0].to(torch.float32).cpu()
+        # pre_conv_temp = pre_conv_temp.permute((1, 2, 0))
+
+        # post_conv_temp = labelMapToIm(pre_conv_temp, [
+        #     [[0, 0, 0], 0],
+        #     [[256, 0, 0], 1],
+        #     [[0, 256, 0], 2],
+        #     [[0, 0, 256], 3],
+        # ])
+        # print("unique vals: ", temp.unique())
+        # save_image(post_conv_temp, "pics/temp.png")
+        # exit()
 
         # img1 = pred[0].argmax(dim=0).to(torch.float32).cpu()/21
         # save_image(img1, 'pics/pred_after_fold.png')
         # exit()
-        return pred, latent
+        # return pred, latent
+        return pred / temp, latent
 
     def forward(self, inp, slide=False):
         resulter, debugger = {}, {}
