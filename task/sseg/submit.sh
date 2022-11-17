2e85542a06fb55389dd68b6931656c838933b031
Jeremiah Coholich <jmcoholich@gmail.com>
2022-11-17 12:01:54 -0500

    add suponly 160 and 224 experiments

--- task/sseg/submit.sh
+++ task/sseg/submit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=suponly_val
+#SBATCH --job-name=suponly_224im
 #SBATCH --gres gpu:4
 #SBATCH --nodes 1
 #SBATCH --cpus-per-gpu 7
 #SBATCH --partition=short
 #SBATCH --exclude=nestor
-#SBATCH --output=slurm_logs/suponly_val.out
-#SBATCH --error=slurm_logs/suponly_val.err
+#SBATCH --output=slurm_logs/suponly_224im.out
+#SBATCH --error=slurm_logs/suponly_224im.err
 #SBATCH --constraint=2080_ti
 
 set -x
@@ -22,6 +22,8 @@ conda activate PixelSSL
 echo "Launching training"
 # python -m script.deeplabv2_pascalvoc_1-16_sslmt
 # python -m script.deeplabv2_pascalvoc_1-16_sslmt_large_batch
-python -m script.deeplabv2_pascalvoc_1-16_suponly
+# python -m script.deeplabv2_pascalvoc_1-16_suponly
+# python -m script.deeplabv2_pascalvoc_1-16_suponly_160im
+python -m script.deeplabv2_pascalvoc_1-16_suponly_224im
 
 
